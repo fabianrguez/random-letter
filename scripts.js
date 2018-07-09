d1781a7c01e33a6d23ee63794b6dea56ff6c5cc5
fabianrodriguezgamez <Fr01041993_118>
2018-07-09 14:47:22 +0100

    refactor code and save letters printed

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
 const generateButton = document.querySelector('.btn-generate');
 const letterDiv = document.querySelector('.letter');
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lettersShowed = [];
 
 function randomLetter() {
   const lettersArray = [...letters];
-  const randomNumber = Math.floor(Math.random() * [...letters].length);
-  letterDiv.textContent = lettersArray[randomNumber];
+  const randomNumber = Math.floor(Math.random() * lettersArray.length);
+  return lettersArray[randomNumber];
 }
 
-generateButton.addEventListener('click', randomLetter);
+function printLetter(letter) {
+  letterDiv.textContent = letter;
+}
+
+function generate() {
+  const letter = randomLetter();
+  printLetter(letter);
+  lettersShowed.push(letter);
+}
+
+generateButton.addEventListener('click', generate);
